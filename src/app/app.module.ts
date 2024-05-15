855415f2c9878b36aa0c76f335390a8580903e77
Elena Vasileva <milena-iron@yandex.ru>
2024-05-15 19:00:44 +0700

    fix: исправить верстку - установить контрастный цвет color: #db7713;

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+
+// import { AppComponent } from './app.component';
+// import { PlanetsComponent } from './components/planets/planets.component';
+// import { PeopleComponent } from './components/people/people.component';
+// import { NotFoundComponent } from './components/not-found/not-found.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     PlanetsComponent,
+//     PeopleComponent,
+//     NotFoundComponent,
+//   ],
+//   imports: [BrowserModule],
+//   providers: [],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
